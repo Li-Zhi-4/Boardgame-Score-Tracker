d8e64f15fa94b90dede1d3c1750a9b41ca78e2ab
Sara Thompson <s34thomp@gmail.com>
2025-06-26 22:15:13 -0400

    Updated error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,7 +1,31 @@
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import { Link } from "react-router-dom"
+
 
 export function ErrorPage() {
     
     return (
-        <p>Oops! This page doesn’t exist.</p>
+        <div className="flex justify-center items-center h-screen w-screen">
+            <Card className="w-[300px]">
+                <CardHeader>
+                    <CardTitle>Error 404</CardTitle>
+                    <CardDescription>Oops! This page doesn’t exist.</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <Button asChild className="w-full">
+                        <Link to="/">Go Home</Link>
+                    </Button>
+                </CardContent>
+            </Card>
+        </div>
     )
 }
